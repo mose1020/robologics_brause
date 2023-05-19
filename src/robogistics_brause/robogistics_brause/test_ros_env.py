a6753438cdfa592c374b0799046ff7fe3a9c465c
mose1020 <sebbi.moser@gmx.de>
2023-05-19 13:57:57 +0200

    testing

--- src/robogistics_brause/robogistics_brause/test_ros_env.py
+++ src/robogistics_brause/robogistics_brause/test_ros_env.py
@@ -4,25 +4,51 @@ from ros_environment.transform import Affine
 import time
 
 def main(args=None):
+
     # initialize ros communications for a given context
     rclpy.init(args=args)
 
     robot = RobotClient(is_simulation=False)
-    time.sleep(2)
-    print("StartTime:",time.asctime( time.localtime(time.time()) ))
-    robot.close_vacuum_gripper()
-    # robot ptp movement again
-    print("AfterCloseTime:",time.asctime( time.localtime(time.time()) ))
-    
+    robot.home_pose = Affine((0.122, -0.052, 1.426), (0.004, 0.860, 0.007, 0.510))
+    # #drop_pose = [(0.109, -0.389, 1.121), (0.497, 0.497, 0.503, 0.503)]
+    #move robot to home pose
+    robot.home()
+    # print("Finished",finished)
 
-    #robot.close_vacuum_gripper()
-    # back to home pose
-    time.sleep(2)
-    print("AfterSleep2:",time.asctime( time.localtime(time.time()) ))
+    time.sleep(1)
+    print("StartTime:",time.asctime( time.localtime(time.time()) ))
+    
+    # # robot ptp movement again
+    print("AfterCloseTime:",time.asctime( time.localtime(time.time()) ))
+    #-----------pick sth from R5 on the grid----------------------------------------------
+    #move to above position
+    robot.lin(Affine((0.024, 0.319, 1.121), (0.444, 0.445, 0.550, 0.550)))
+    robot.close_vacuum_gripper()
+    time.sleep(0.1)
+
+    # # move down
+    robot.lin(Affine((0.024, 0.319, 1.021), (0.444, 0.445, 0.550, 0.550)))
+    time.sleep(0.1)
+
+    # #move up again and to the drop position
+    robot.lin(Affine((0.024, 0.319, 1.121), (0.444, 0.445, 0.550, 0.550)))
+    # time.sleep(0.1)
+    
+    # #drop position
+    robot.ptp(Affine((0.109, -0.389, 1.221), (0.497, 0.497, 0.503, 0.503)))
+    # time.sleep(5)
+
+    
+    
+    # time.sleep(1)
+    # print("AfterSleep2:",time.asctime( time.localtime(time.time()) ))
     robot.open_vacuum_gripper()
-   
+    time.sleep(1)
+    #back to home pose
+    robot.home()
+    time.sleep(2)
     print('EndTime:',time.asctime( time.localtime(time.time()) ))
-    # destroy the robot node
+    # # destroy the robot node
     
     robot.destroy_node()
     # shutdown previously initialized context
--- src/robogistics_brause/setup.py
+++ src/robogistics_brause/setup.py
@@ -26,7 +26,8 @@ setup(
             # 'geometrie_pub = r2e_demos.geometrie_pub:main',
             # 'geometrie_pub_2 = r2e_demos.geometrie_pub_2:main',
             # 'geometrie_sub = r2e_demos.geometrie_sub:main'
-            'test_ros_env = robogistics_brause.test_ros_env:main'
+            'test_ros_env = robogistics_brause.test_ros_env:main',
+            'test_ros_env_copy = robogistics_brause.test_ros_env_copy:main'
         ],
     },
 )
