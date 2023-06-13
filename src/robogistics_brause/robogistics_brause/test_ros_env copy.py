83cbba42f3bb2b5eeb26ad02b90480bba55d8ad9
Stephan <klst1026@h-ka.de>
2023-06-13 15:00:35 +0200

    robot to class

--- src/robogistics_brause/robogistics_brause/test_ros_env copy.py
+++ src/robogistics_brause/robogistics_brause/test_ros_env copy.py
@@ -1,7 +1,6 @@
 import rclpy
 from ros_environment.scene import RobotClient
 from ros_environment.transform import Affine
-from object_detection.
 import time
 class BrausePicker:
     def __init__(
@@ -73,26 +72,61 @@ class BrausePicker:
         self.robot.ptp(Affine((0.109, -0.389, 1.221), (0.497, 0.497, 0.503, 0.503)))
         self.robot.open_vacuum_gripper()
         #time.sleep(1.0)
-    def shutdown(self) -> None:
-        """ Finish up the demo by destroying the RobotClient node.
-            
-        """
-        self.robot.destroy_node()
+
 
 def main(args=None):
 
     # initialize ros communications for a given context
     rclpy.init(args=args)
-    # create a new demo instance
-    BrausePicker = BrausePicker(is_simulation=False)
 
-    # user chooses color and if its available the robot picks it otherwise new color is chosen
-    pose_from_camerea = 
+    robot = RobotClient(is_simulation=False)
+    robot.home_pose = Affine((0.122, -0.052, 1.426), (0.004, 0.860, 0.007, 0.510))
+    # #drop_pose = [(0.109, -0.389, 1.121), (0.497, 0.497, 0.503, 0.503)]
 
+    #move robot to home pose
+    robot.home()
+    # print("Finished",finished)
 
+    time.sleep(1)
+    #print("StartTime:",time.asctime( time.localtime(time.time()) ))
+    
+    # # robot ptp movement again
+    #print("AfterCloseTime:",time.asctime( time.localtime(time.time()) ))
+
+    #-----------pick sth from R5 on the grid----------------------------------------------
+    #move to above position
+    robot.lin(Affine((0.024, 0.319, 1.121), (0.444, 0.445, 0.550, 0.550)))
+    robot.close_vacuum_gripper()
+    time.sleep(0.1)
+
+    # # move down
+    robot.lin(Affine((0.024, 0.319, 1.021), (0.444, 0.445, 0.550, 0.550)))
+    time.sleep(0.1)
+
+    # #move up again and to the above position
+    robot.lin(Affine((0.024, 0.319, 1.121), (0.444, 0.445, 0.550, 0.550)))
+    time.sleep(0.1)
+    
+    #check in camera
+    robot.ptp(Affine((-0.126, 0.270, 1.503), (-0.554, 0.502, -0.436, 0.501)))
+    time.sleep(0.1)
+    # #drop position
+    robot.ptp(Affine((0.109, -0.389, 1.221), (0.497, 0.497, 0.503, 0.503)))
+    # time.sleep(5)
 
     
-    BrausePicker.shutdown_demo()
+    
+    # time.sleep(1)
+    # print("AfterSleep2:",time.asctime( time.localtime(time.time()) ))
+    robot.open_vacuum_gripper()
+    time.sleep(1)
+    #back to home pose
+    robot.home()
+    time.sleep(2)
+    #print('EndTime:',time.asctime( time.localtime(time.time()) ))
+    # # destroy the robot node
+    
+    robot.destroy_node()
     # shutdown previously initialized context
     rclpy.shutdown()
 
